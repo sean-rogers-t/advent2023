cdf97166d41b720ab949caada08879fc9c0cfa81
Sean Rogers <rogers.sean.tyler@gmail.com>
2024-01-05 18:54:10 -0600

    Part 2 solution

--- Day17.py
+++ Day17.py
@@ -3,7 +3,7 @@ import numpy as np
 import heapq
 
 
-def dijkstra(grid,max_steps):
+def dijkstra(grid,min_steps,max_steps):
     rows, cols = len(grid), len(grid[0])
     dest = (rows - 1, cols - 1)
 
@@ -22,11 +22,29 @@ def dijkstra(grid,max_steps):
 
         # Stop if we reach the destination
         if (row, col) == dest:
-            print(dist)
             break
         if (row, col,direction,step_count) in passed:
             continue
         passed.add((row, col,direction,step_count))
+        if direction==-1:
+            for i in range(2):
+                direction=i
+                dr,dc=directions[direction]
+                r, c = row + dr, col + dc
+                step_count=1
+                new_dist = dist + grid[r][c]
+                new_node = (r, c,direction,step_count)
+                heapq.heappush(queue, (new_dist, new_node))
+            continue
+        if step_count<min_steps:
+            dr,dc= directions[direction]
+            r, c = row + dr, col + dc
+            if 0 <= r < rows and 0 <= c < cols:
+                new_step_count = step_count + 1 if direction!=-1 else 1
+                new_dist = dist + grid[r][c]
+                new_node = (r, c,direction,new_step_count)
+                heapq.heappush(queue, (new_dist, new_node))
+            continue
         for i,(dr, dc) in enumerate(directions):
             if i == (direction + 2) % 4 and direction!=-1:  # Avoid backtracking
                 continue
@@ -45,7 +63,7 @@ def dijkstra(grid,max_steps):
 with open('day17input.txt') as f:
     grid = [[int(x) for x in line.strip()] for line in f]
 # Find the shortest path and its cost
-shortest_path_cost= dijkstra(grid,3)
+shortest_path_cost= dijkstra(grid,4,10)
 print("Shortest path cost:", shortest_path_cost)
 
 
