9d6b619b72d76f3c7462dc9c75afcd7813334eaa
Sean Rogers <rogers.sean.tyler@gmail.com>
2024-01-06 15:43:40 -0600

    got rid of overflow issues from numpy, Part 2 solution

--- Day 18.py
+++ Day 18.py
@@ -1,32 +1,38 @@
 import numpy as np
 
+def ShoeLaceFormula(vertices):
+    sum1=0
+    
+    for i in range(len(vertices)-1):
+        sum1+=int(vertices[i][0])*int(vertices[i+1][1])-int(vertices[i+1][0])*int(vertices[i][1])
+        
+    sum1=0.5*np.abs(sum1)
+    return sum1
+
 directionDictionary={"R":np.array([0,1]),"D":np.array([1,0]),"L":np.array([0,-1]),"U":np.array([-1,0])}
+hexDirectionDictionary={"0":np.array([0,1]),"1":np.array([1,0]),"2":np.array([0,-1]),"3":np.array([-1,0])}
 
 with open("day18input.txt") as f:
      directions = [(directionDictionary[direction],int(distance),color.replace('(', '').replace(')', '')) for direction,distance,color in [line.strip().split() for line in f.readlines()]]
 
 simple_directions = [(direction,distance) for direction,distance,color in directions]
+hex_directions = [(hexDirectionDictionary[color[-1]],int(color[1:-1],16)) for direction,distance,color in directions]
+
 vertices=[]
 prevVertex=np.array([0,0])
 vertices.append(prevVertex)
-for direction,distance in simple_directions:
+
+for direction,distance in hex_directions:
     vertex = prevVertex+(distance)*direction
     vertices.append(vertex)
     prevVertex=vertex
-print([tuple(vertex) for vertex in vertices])
-
-def ShoeLaceFormula(vertices):
-    sum1=0
-    for i in range(len(vertices)-1):
-        sum1+=vertices[i][0]*vertices[i+1][1]-vertices[i+1][0]*vertices[i][1]
-    sum1=0.5*np.abs(sum1)
-    vertices=np.array(vertices)
-    sum2=0.5*np.abs(np.dot(vertices[:,0],np.roll(vertices[:,1],-1))-np.dot(vertices[:,1],np.roll(vertices[:,0],-1)))
-    return sum2
+#Use shoelace formula to calculate area
 area=ShoeLaceFormula(vertices)
-perimeter= sum(map(lambda x: x[1], simple_directions))
+perimeter= sum(map(lambda x: x[1], hex_directions))
 
 #Pick's theorem
 i=area-1/2*perimeter+1
+#Since the perimeter is thick total area is interior area + perimeter
 totalArea = i +perimeter
+
 print(totalArea)
