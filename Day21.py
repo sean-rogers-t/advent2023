85dcb0631ea290c629ef7cb2fb950f844728924e
Sean Rogers <rogers.sean.tyler@gmail.com>
2024-01-08 12:55:37 -0600

    Much faster solution using parity argument to never cross same point twice

--- Day21.py
+++ Day21.py
@@ -5,29 +5,7 @@ def printGrid(grid):
         print("".join(row))
     print()
 
-def bfs_paths(grid, starts, current_length, path_length):
-    if current_length == path_length:
-        return (starts, len(starts))
-    rows, cols = len(grid), len(grid[0])
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # Up, right, down, left
-    visited = set()
-    queue = deque([(start, current_length) for start in starts])  # Queue of (position, current path length)
-    paths_end_points = []
 
-    while queue:
-        (row, col), length = queue.popleft()
-        if length == current_length + 1:
-            if (row, col) not in paths_end_points:
-                paths_end_points.append((row, col))
-            continue
-
-        for dr, dc in directions:
-            r, c = row + dr, col + dc
-            if 0 <= r < rows and 0 <= c < cols and grid[r][c] != '#': #  and (r, c) not in visited
-                visited.add((r, c))
-                queue.append(((r, c), length + 1))
-
-    return bfs_paths(grid, paths_end_points, current_length + 1, path_length)
 
 # Read the grid from file
 with open('day21input.txt') as f:
@@ -35,8 +13,23 @@ with open('day21input.txt') as f:
 
 # Find starting point 'S'
 start = next((r, c) for r, row in enumerate(grid) for c, val in enumerate(row) if val == 'S')
+path_length = 64
+queue = deque([(start, path_length)])
+seen  = set()
+seen.add(start)
+end_points=set()
+directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+rows, cols = len(grid), len(grid[0])
+while queue:
+    (row, col), length = queue.popleft()
+    if length % 2==0:
+        end_points.add((row,col))
 
-# Find all end points of paths of length 6 starting at 'S'
-end_points, solution= bfs_paths(grid, [start],0,64)
+    for dr, dc in directions:
+        r, c = row + dr, col + dc
+        if 0 <= r < rows and 0 <= c < cols and grid[r][c] != '#' and (r,c) not in seen and length>0: #  and (r, c) not in visited
+            seen.add((r, c))
+            queue.append(((r, c), length - 1))
+
 #print("End points of all paths of length 6 starting at 'S':", end_points)
-print("Number of paths of length 6 starting at 'S':", solution)
+print("Number of paths of length 6 starting at 'S':", len(end_points))
